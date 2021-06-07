2e339cc0fe3bf907b83f8b110277cabb08e10a05
nihalattar14 <nihalattar14@gmail.com>
2021-06-07 12:02:17 +0530

    Wrapping the code

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -8,15 +8,17 @@ def index(request):
 def analyze(request):
 
     # GET the text
-    djtext = request.GET.get('text','default')
+    djtext = request.POST.get('text','default')
 
     #checkbox values
 
-    removepunc = request.GET.get('removepunc','off')
-    fullcaps = request.GET.get('fullcaps','off')
-    newlineremover = request.GET.get('newlineremover','off')
-    extraspaceremover = request.GET.get('extraspaceremover','off')
-    charcount = request.GET.get('charcount','off')
+    removepunc = request.POST.get('removepunc','off')
+    fullcaps = request.POST.get('fullcaps','off')
+    newlineremover = request.POST.get('newlineremover','off')
+    extraspaceremover = request.POST.get('extraspaceremover','off')
+    charcount = request.POST.get('charcount','off')
+
+    params = {}
 
 
     #check with checkbox is on
@@ -27,61 +29,46 @@ def analyze(request):
             if char not in punctuations:
                 analyzed = analyzed + char
         params = {'Purpose':'Removed Punctuations','analyzed_text':analyzed}
+        djtext = analyzed
         #Analyze the text
-        return render(request, 'analyze.html', params)
+        # return render(request, 'analyze.html', params)
 
-    elif(fullcaps == 'on'):
+    if(fullcaps == 'on'):
         analyzed = djtext.upper()
         params = {'Purpose': 'Changed to Upper case', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
 
-    elif(newlineremover == 'on'):
+    if(newlineremover == 'on'):
         analyzed = ""
         for char in djtext:
-            if char != "\n":
+            if char != "\n" and char != "\r":
                 analyzed = analyzed + char
         params = {'Purpose': 'Remove New Lines ', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
 
-    elif(extraspaceremover == 'on'):
+    if(extraspaceremover == 'on'):
         analyzed = ""
         for index, char in enumerate(djtext):
             if not(djtext[index] == " " and djtext[index+1] == " "):
               analyzed = analyzed + char
 
         params = {'Purpose': 'Changed to Upper case', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
 
-    elif(charcount == 'on'):
+    if(charcount == 'on'):
         analyzed = 0
         for i in range(0, len(djtext)):
             if(djtext[i]!=''):
                 analyzed = analyzed + 1
         params = {'Purpose': 'Changed to Upper case', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
+        # return render(request, 'analyze.html', params)
+
+    if(removepunc!="on" and fullcaps!="on" and newlineremover!="on" and extraspaceremover!="on" and charcount!="on"):
+        return HttpResponse("Please select atleast one operation ")
+
+    return render(request, 'analyze.html', params)
 
 
-    else:
-        return HttpResponse("Please Select atleast any one checkbox")
-
-
-
-# def removepunc(request):
-#
-#     # GET the text
-#     djtext = request.GET.get('text','default')
-#     print(djtext)
-#     #Analyze the text
-#     return HttpResponse("remove punc")
-#
-# def capfirst(request):
-#     return HttpResponse("capitalize first")
-#
-# def newlineremove(request):
-#     return HttpResponse("newline remove")
-#
-# def spaceremove(request):
-#     return HttpResponse("space remove<a href = '/'>Back</a>")
-#
-# def charcount(request):
-#     return HttpResponse("char count")
